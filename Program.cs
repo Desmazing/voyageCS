ab736e685d596b9447818f815ceeefba59909729
Desmond O <100370067+Desmazing@users.noreply.github.com>
2022-04-07 19:53:54 +0300

    hello_world

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// baby steps with C#
+using System;
+
+namespace myproject
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
