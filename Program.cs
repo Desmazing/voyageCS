252e96b919b7c16ab5119c351fb8aad1a1de1933
Desmond O <100370067+Desmazing@users.noreply.github.com>
2022-04-07 20:05:40 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -8,6 +8,13 @@ namespace myproject
         static void Main(String[] args)
         {
             Console.WriteLine("Hello, World!");
+            string fname, lname;
+            Console.WriteLine("Enter your first name: ");
+            fname = Console.ReadLine();
+            Console.WriteLine("Enter your last name: ");
+            lname = Console.ReadLine();
+            Console.Write("Your full name is "+fname+" "+lname);
+
         }
     }
 }
